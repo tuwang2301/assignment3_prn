3e89ba7083d8d388e167d87feca4e832cc4deacc
BuiVanToanHE163899 <121929705+BuiVanToanHE163899@users.noreply.github.com>
2024-06-24 08:16:02 +0700

    [toan] Code Order Feature

---
+++ Assignment1_Client/Controllers/MyOrderController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment1_Client.Controllers
+{ 
+    public class MyOrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Detail()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
